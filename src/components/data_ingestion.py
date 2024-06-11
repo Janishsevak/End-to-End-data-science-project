c62c8c2169dc60347bd047622d54d6ea0ca30067
Janishsevak <jenishsevak7@gmail.com>
2024-06-11 22:29:19 +0530

    data_ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,33 +8,36 @@ from sklearn.model_selection import train_test_split
 
 
 from dataclasses import dataclass
-
 @dataclass
-class DataIngestionConfing:
-    train_data_path : str = os.path.join('artifact','train.csv')
-    test_data_path : str = os.path.join('artifact','test.csv')
-    raw_data_path : str = os.path.join('artifact','raw.csv')
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifact','train.csv')
+    test_data_path:str=os.path.join('artifact','test.csv')
+    raw_data_path:str=os.path.join('artifact','raw.csv')
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfing()
+        self.ingestion_config=DataIngestionConfig()
 
-    def initite_data_ingestion(self):
+    def initiate_data_ingestion(self):
         try:
-            df = read_sql_data()
+            ##reading the data from mysql
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("Reading completed mysql database")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Data Ingestion completed")
+            logging.info("Data Ingestion is completed")
 
-            return (self.ingestion_config.train_data_path,
-                    self.ingestion_config.test_data_path)
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+
+            )
         except Exception as e:
             raise CustomException(e, sys)
